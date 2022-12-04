4e475967173260ae13e692e8d68a1d24562666eb
e8252125 <18252125@life.hkbu.edu.hk>
2022-12-04 18:48:43 +0800

    upload modal to ipfs

--- src/Dialogs/Input.jsx
+++ src/Dialogs/Input.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext, useRef, useEffect} from 'react';
+import React, {useState, useContext, useRef, useEffect, Suspense} from 'react';
 import {FileUpload} from 'react-ipfs-uploader'
 import {create} from 'ipfs-http-client';
 import * as IPFS from 'ipfs-core'
@@ -15,38 +15,42 @@ import {
 } from '@mui/material';
 import Stack from "@mui/material/Stack";
 import Typography from '@mui/material/Typography';
+import {OrbitControls} from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
 
+import Model from "../Components/Model";
+
+
+const authorization = "Basic " + btoa(process.env.REACT_APP_PROJECT_ID + ":" + process.env.REACT_APP_PROJECT_SECRET);
+const ipfs = create({
+    url: "https://ipfs.infura.io:5001/api/v0",
+    headers: {
+        authorization
+    }
+})
 
 export default function InputDialog(props) {
 
-    const [isReading, setIsReading] = useState(false);
-    const [fileContent, setFileContent] = useState(null);
+    const [isLoadinging, setIsLoadinging] = useState(false);
+
+    const [file, setFile] = useState(null);
 
     const uploadToIpfs = async () => {
-        if (fileContent) {
-            const ipfs = await IPFS.create()
-            const addResponse = await ipfs.add(fileContent)
-            console.info(addResponse)
+        if (file) {
+            setIsLoadinging(true);
+            const response = await ipfs.add(file);
+            console.log(response);
+            const token = response.path
+            console.log(token)
+            setIsLoadinging(false);
         }
     }
 
-    const handleFileReader = async (e) => {
-        console.log("reading")
-        setIsReading(true);
-        try {
-            const file = e.target.files[0];
-            const reader = new FileReader();
-            reader.onload = function (e) {
-                console.log(e.target.result)
-                setFileContent(e.target.result)
-                setIsReading(false);
-                console.log("finish reading")
-            }
-            reader.readAsText(file)
-        } catch (error) {
-            console.log(error)
-        }
-    };
+    const genPreview = () => {
+        setIsLoadinging(true);
+
+        setIsLoadinging(false);
+    }
 
 
     return (
@@ -55,16 +59,31 @@ export default function InputDialog(props) {
             <DialogContent>
                 <Stack spacing={1}>
                     <form>
-                        <input type="file" onChange={handleFileReader}/>
+                        <input type="file" onChange={(e) => setFile(e.target.files[0])}/>
                     </form>
-                    <Typography variant="h6" fontWeight="bold" sx={{mt:0}}>Preview: </Typography>
-                    {isReading ? <CircularProgress/> : fileContent}
+                    <Typography variant="h6" fontWeight="bold" sx={{mt: 0}}>Preview: </Typography>
+                    {isLoadinging ? <CircularProgress/> :
+                        <Canvas
+                            camera={{position: [2, 0, 12], fov: 10}}
+                            style={{width: '33vw', height: '70vh'}}
+                        >
+                            <ambientLight intensity={1.25}/>
+                            <ambientLight intensity={0.1}/>
+                            <directionalLight intensity={1}/>
+                            <Suspense fallback={null}>
+                                <Model position={[0.025, -0.9, 0]}/>
+                            </Suspense>
+                            <OrbitControls/>
+                        </Canvas>
+                    }
                 </Stack>
 
             </DialogContent>
-            <DialogActions>
+            <DialogActions sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Button variant="outlined" onClick={() => genPreview()}
+                        disabled={!file}>Generate Preview</Button>
                 <Button variant="outlined" onClick={() => uploadToIpfs()}
-                        disabled={!fileContent || isReading}>Submit</Button>
+                        disabled={!file || isLoadinging}>Upload</Button>
             </DialogActions>
         </Dialog>
     );
