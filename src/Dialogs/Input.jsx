2d4e3ac8c31c0d9d5654506f2322342116bc618e
e8252125 <18252125@life.hkbu.edu.hk>
2022-12-04 17:28:16 +0800

    upload file to ipfs

--- src/Dialogs/Input.jsx
+++ src/Dialogs/Input.jsx
@@ -1,43 +1,70 @@
-import React, {useState, useContext, useRef} from 'react';
+import React, {useState, useContext, useRef, useEffect} from 'react';
 import {FileUpload} from 'react-ipfs-uploader'
 import {create} from 'ipfs-http-client';
 import * as IPFS from 'ipfs-core'
 
-import Typography from '@mui/material/Typography';
-import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} from '@mui/material';
+import {
+    Box,
+    Button,
+    CircularProgress,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField
+} from '@mui/material';
 import Stack from "@mui/material/Stack";
+import Typography from '@mui/material/Typography';
 
-// const ipfsClient = create()
 
 export default function InputDialog(props) {
 
-    const [fileUrl, setFileUrl] = useState('');
-    const [selectedFile, setSelectedFile] = useState(undefined);
+    const [isReading, setIsReading] = useState(false);
+    const [fileContent, setFileContent] = useState(null);
 
-    // const file = useRef()
-
-    const getModel = async () => {
-        console.log(selectedFile)
-
-        const ipfs = await IPFS.create()
-        const addResponse = await ipfs.add(selectedFile)
-        console.info(addResponse)
-
+    const uploadToIpfs = async () => {
+        if (fileContent) {
+            const ipfs = await IPFS.create()
+            const addResponse = await ipfs.add(fileContent)
+            console.info(addResponse)
+        }
     }
+
+    const handleFileReader = async (e) => {
+        console.log("reading")
+        setIsReading(true);
+        try {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                console.log(e.target.result)
+                setFileContent(e.target.result)
+                setIsReading(false);
+                console.log("finish reading")
+            }
+            reader.readAsText(file)
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
 
     return (
         <Dialog open={props.open} onClose={props.handleClose}>
-            <DialogTitle>
-                Upload your model
-            </DialogTitle>
+            <DialogTitle>Upload your model</DialogTitle>
             <DialogContent>
-                <form>
-                    <input type="file" value={"test"}
-                           onChange={(e) => setSelectedFile(e.target.files[0])}/>
-                </form>
+                <Stack spacing={1}>
+                    <form>
+                        <input type="file" onChange={handleFileReader}/>
+                    </form>
+                    <Typography variant="h6" fontWeight="bold" sx={{mt:0}}>Preview: </Typography>
+                    {isReading ? <CircularProgress/> : fileContent}
+                </Stack>
+
             </DialogContent>
             <DialogActions>
-                <Button variant="outlined" onClick={() => getModel()}>Submit</Button>
+                <Button variant="outlined" onClick={() => uploadToIpfs()}
+                        disabled={!fileContent || isReading}>Submit</Button>
             </DialogActions>
         </Dialog>
     );
