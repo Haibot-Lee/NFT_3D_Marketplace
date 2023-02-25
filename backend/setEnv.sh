b9bc700d067f5424c217d333f0ddc3f49958e073
Haibot-Lee <61733292+Haibot-Lee@users.noreply.github.com>
2023-02-25 13:42:59 +0000

    Deploying to gh-pages from @ Haibot-Lee/NFT_3D_Marketplace@7a244483335433d5ccb5ab5cad2beb743e1d7789 🚀

--- backend/setEnv.sh
+++
@@ -1,9 +0,0 @@
-cp .env env.tmp
-npm install
-npx hardhat compile
-echo -e `npx hardhat run scripts/deploy.js` >> env.tmp
-mv env.tmp ../.env
-
-mkdir ../src/contracts
-cp artifacts/contracts/MarketPlace.sol/MarketPlace.json ../src/contracts/MarketPlace.json
-cp artifacts/contracts/NFT.sol/NFT.json ../src/contracts/NFT.json
