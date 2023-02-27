d443f66cb87755a9374b14cd7beac890f1a86bd4
e8252125 <18252125@life.hkbu.edu.hk>
2023-02-27 20:23:35 +0800

    add space

--- src/Metaverse/Space.jsx
+++ src/Metaverse/Space.jsx
@@ -1,9 +1,10 @@
 import React, {Suspense} from 'react';
-import sky from '../bg.jpeg'
-import gallery from '../models/space.glb'
 
+import sky from './bg.jpeg'
+import gallery from './assets/space.glb'
 import 'aframe';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import Model from './Model'
 
 
 export default function Space(props) {
@@ -11,7 +12,6 @@ export default function Space(props) {
     loader.load(gallery, (d) => {
         const entity = document.getElementById("gallery");
         entity.object3D.add(d.scene);
-
     });
 
     return (
@@ -19,15 +19,13 @@ export default function Space(props) {
             <a-assets>
                 <img id="sky" src={sky}/>
             </a-assets>
-
-            <a-sky
-                color="#FFFFFF"
-                material={"src: sky"}
-                rotation="0 0 0"
-            >
-
-            </a-sky>
+            <a-sky color="#FFFFFF"
+                   material={"src: #sky"}
+                   rotation="0 0 0"/>
             <a-entity id="gallery" position="0 0 0"></a-entity>
+
+            <a-box position="5 5 3" rotation="0 45 0" color="#4CC3D9"></a-box>
+            <Model token={'QmZSZ5ooNwbqd8MX42nd6eqG3WzmqxLUb3DdYkjHyLCC4K'} x={5} y={0} z={5}/>
         </a-scene>
     );
 }
